3c2ae7cfad40d34b63e130339bc38d387d73ee28
Edvaldo José <edvaldojose003@gmail.com>
2023-10-22 20:40:47 -0300

    Commit Projeto-java-api-rest

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-  public static void main(String[] args) {
-
-    System.out.println("Hello and welcome!");
-      System.out.println("Bem-vindo ao meu Projeto Api Rest!");
-    }
-}
